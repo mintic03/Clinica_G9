01d17873d5b387e29b04b27dc45959ba6ca4ed83
Julian-DSP <judasanpa@gmail.com>
2022-09-13 23:42:16 -0500

    Avance

--- clinica/clinicaAPI/models.py
+++ clinica/clinicaAPI/models.py
@@ -1,3 +1,86 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model): #Primer Tabla Personas
+	id = models.CharField(max_length = 20, primary_key = True)
+	Nombre_Enf = models.CharField(max_length = 45)
+	Apellido_Enf = models.CharField(max_length = 45)
+	Telefono_Enf = models.CharField(max_length = 45)
+	Genero = [
+		('F','Femenino'),
+		('M','Masculino')
+	]
+	Genero = models.CharField(max_length = 1, choices = Genero, default ='F')
+
+	def nombrecompleto(self): # Funcion para Nombre
+		txt="{0}, {1} {2}"
+		return txt.format(self.id, self.Nombre_Enf, self.Apellido_Enf)
+
+	def __str__(self):
+		txt="{0}"
+		return txt.format(self.nombrecompleto())
+
+class Medico(models.Model):
+	id_medico = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+	Especialidad = models.CharField(max_length = 50)
+	Registro = models.CharField(max_length = 45)
+
+	def nombrecompleto_M(self): # Funcion para Nombre
+		txt="{0}, Especialidad: {1}"
+		return txt.format(self.id_medico.nombrecompleto(), self.Especialidad)
+
+	def __str__(self):
+		txt="Medico: {0}"
+		return txt.format(self.nombrecompleto_M())
+
+
+class Enfermero(models.Model):
+	id_enfermero = models.ForeignKey(Persona, null = False, blank = False, on_delete = models.CASCADE)
+
+	def __str__(self):
+		txt="Enfermero: {0}"
+		return txt.format(self.id_enfermero.nombrecompleto())
+
+class Paciente(models.Model):
+	id_paciente = models.ForeignKey(Persona, null=False, blank=False, on_delete=models.CASCADE)
+	Direccion = models.CharField(max_length = 50)
+	Ciudad = models.CharField(max_length = 45)
+	Fecha_Nacimiento = models.DateField()
+	Latitud = models.CharField(max_length = 50)
+	Longitud = models.CharField(max_length = 50)
+	Medicos_id = models.ForeignKey(Medico, null=False, blank=False, on_delete=models.CASCADE)
+
+	def nombrecompleto_P(self): # Funcion para Nombre
+		txt="{0} "
+		return txt.format(self.id_paciente.nombrecompleto())
+
+	def __str__(self):
+		txt="Paciente: {0} / Medico: {1}"
+		return txt.format(self.nombrecompleto_P(), self.Medicos_id.nombrecompleto_M())
+
+class Familiar(models.Model):
+	id_familiar = models.ForeignKey(Persona, null = False, blank = False, on_delete = models.CASCADE)
+	Parentesco = [
+		('F','Padre/Madre'),
+		('S','Hijo/Hija'),
+		('B','Hermano/Hermana'),
+		('G','Abuelo/Abuela'),
+		('O','Otro')
+	]
+	Parentesco = models.CharField(max_length = 1, choices = Parentesco, default ='F')
+	E_mail = models.CharField(max_length = 45)
+	Paciente_id = models.ForeignKey(Paciente, null=False, blank=False, on_delete=models.CASCADE)
+
+	def __str__(self):
+		txt = "Familiar: {0} / Paciente: {1}"
+		return txt.format(self.id_familiar.nombrecompleto(), self.Paciente_id.nombrecompleto_P())
+
+class Signos_vitale(models.Model):
+	id_paciente = models.ForeignKey(Paciente, null = False, blank = False, on_delete = models.CASCADE)
+	Tipo_signo_vital = models.CharField(max_length = 70)
+	Valor_signo_vital = models.CharField(max_length = 45)
+	Fecha = models.DateField()
+
+	def __str__(self):
+		txt = "Signos vitales: {0}"
+		return txt.format(self.id_paciente.nombrecompleto_P())
--- clinica/clinicaAPI/views.py
+++ clinica/clinicaAPI/views.py
@@ -1,3 +1,21 @@
+from ast import Return
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Hola Mundo")
+
+def consultarPaciente(request):
+    if request.method == 'GET':
+        return HttpResponse("Aqui se muestra la informacion de un paciente")
+    else:
+        return HttpResponseNotAllowed(['GET'],"Metodo Invalido")
+    
+
+def agregarPersona(request):
+    if request.method == 'POST':
+        return HttpResponse("Se va a agregar un nuevo miembro")
+    else:
+        return HttpResponseNotAllowed(['POST'],"Metodo Invalido")
+
+
